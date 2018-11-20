dcef9739eb0ee183b1b962a070d36812bcb1c7e7
KangHyoMun12 <leovthat215@gmail.com>
2018-11-20 14:59:18 +0700

    Add files via upload

--- logger/log.go
+++ logger/log.go
@@ -13,17 +13,22 @@ var Logger seelog.LoggerInterface
 
 func loadAppConfig() {
 	appConfig := `
-	<?xml version="1.0" encoding="UTF-8"?>
 	<seelog minlevel="warn">
-		<outputs formatid="common">
-			<rollingfile type="size" filename="./logs/roll.log" maxsize="100000" maxrolls="5"/>
-		</outputs>
-		<formats>
-			<format id="common" format="%Date/%Time [%LEV] %Msg%n" />
-			<format id="critical" format="%File %FullPath %Func %Msg%n" />
-			<format id="criticalemail" format="Critical error on our server!\n    %Time %Date %RelFile %Func %Msg \nSent by Seelog"/>
-		</formats>
-	</seelog>
+    <outputs formatid="common">
+        <rollingfile type="size" filename="/data/logs/roll.log" maxsize="100000" maxrolls="5"/>
+		<filter levels="critical">
+            <file path="/data/logs/critical.log" formatid="critical"/>
+            <smtp formatid="criticalemail" senderaddress="astaxie@gmail.com" sendername="ShortUrl API" hostname="smtp.gmail.com" hostport="587" username="mailusername" password="mailpassword">
+                <recipient address="xiemengjun@gmail.com"/>
+            </smtp>
+        </filter>
+    </outputs>
+    <formats>
+        <format id="common" format="%Date/%Time [%LEV] %Msg%n" />
+	    <format id="critical" format="%File %FullPath %Func %Msg%n" />
+	    <format id="criticalemail" format="Critical error on our server!\n    %Time %Date %RelFile %Func %Msg \nSent by Seelog"/>
+    </formats>
+</seelog>
 	`
 	logger, err := seelog.LoggerFromConfigAsBytes([]byte(appConfig))
 	if err != nil {
