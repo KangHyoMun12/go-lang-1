4c0892ebaa95a8b827322f05d8c303baafe0feb4
KangHyoMun12 <leovthat215@gmail.com>
2018-11-20 11:09:22 +0700

    Add files via upload

--- logger/log.go
+++ logger/log.go
@@ -12,12 +12,24 @@ Logger log
 var Logger seelog.LoggerInterface
 
 func loadAppConfig() {
-	// appConfig, err := os.Open("log.xml")
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	//seelog.LoggerFromConfigAsBytes([]byte(appConfig))
-	logger, err := seelog.LoggerFromConfigAsFile("log.xml")
+	appConfig := `<seelog minlevel="warn">
+    <outputs formatid="common">
+        <rollingfile type="size" filename="/data/logs/roll.log" maxsize="100000" maxrolls="5"/>
+		<filter levels="critical">
+            <file path="/data/logs/critical.log" formatid="critical"/>
+            <smtp formatid="criticalemail" senderaddress="leovthat215@gmail.com" sendername="ShortUrl API" hostname="smtp.gmail.com" hostport="587" username="mailusername" password="mailpassword">
+                <recipient address="nvthat@tma.com.vn"/>
+            </smtp>
+        </filter>
+    </outputs>
+    <formats>
+        <format id="common" format="%Date/%Time [%LEV] %Msg%n" />
+	    <format id="critical" format="%File %FullPath %Func %Msg%n" />
+	    <format id="criticalemail" format="Critical error on our server!\n    %Time %Date %RelFile %Func %Msg \nSent by Seelog"/>
+    </formats>
+</seelog>`
+	logger, err := seelog.LoggerFromConfigAsBytes([]byte(appConfig))
+	// logger, err := seelog.LoggerFromConfigAsFile("./logger/log.xml")
 	if err != nil {
 		fmt.Println(err)
 		return
