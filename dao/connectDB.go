f04165496fb7d58c8e62ec08eafa74c43a5520bc
KangHyoMun12 <leovthat215@gmail.com>
2018-11-27 20:53:34 +0700

    Add files via upload

--- dao/connectDB.go
+++ dao/connectDB.go
@@ -9,14 +9,16 @@ import (
 
 const cmdSelect string = "SELECT * FROM "
 
+/*
+ConnectDB type
+*/
 func ConnectDB() *sql.DB {
 	urlconnect := "thonghv:Thong2012@X@(db4free.net:3306)/db_demo"
 	db, err := sql.Open("mysql", urlconnect)
 	if err != nil {
-		//db, err = sql.Open("mysql", "root:@(127.0.0.1:3306)/trees")
+		db, err = sql.Open("mysql", "root:@(127.0.0.1:3306)/trees")
 		checkerr(err)
 	}
-	checkerr(err)
 	return db
 }
 
